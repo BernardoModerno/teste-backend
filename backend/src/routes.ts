54efbe8fd033c9c309c632789104057c53f2857d
BernardoModerno <bernardo.moderno@gmail.com>
2022-05-16 23:07:28 -0300

    Rotas e Server

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,5 +1,20 @@
 import { Router } from 'express';
+import { ListDadosDoPlanoController } from './controllers/dadosdoplano/ListDadosDoPlanoController';
+import { FindByIdExamesController } from './controllers/exames/FindByIdExamesController';
+import { ListPacientesController } from './controllers/paciente/ListPacientesController';
+
+import uploadConfig from './config/multer'
+import multer from 'multer';
 
 const router = Router();
+
+const upload = multer(uploadConfig.upload("./tmp"));
+
+router.get('/dadosdoplano', new ListDadosDoPlanoController().handle )
+
+router.get('/pacientes', new ListPacientesController().handle )
+
+router.get('/findByIdExames', new FindByIdExamesController().handle )
+
 
 export { router }; 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,4 +11,23 @@ app.use(cors());
 
 app.use(router);
 
+app.use(
+  '/files',
+  express.static(path.resolve(__dirname, '..', 'tmp'))
+)
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if(err instanceof Error){
+    return res.status(400).json({
+      error: err.message
+    })
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error.'
+  })
+
+})
+
 app.listen(3333, () => console.log('Servidor online!!!!'))
